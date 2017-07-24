8dc18eceb960ebb87d91f58d13cdba145d6b1324
Nate <n_straus3@outlook.com>
2017-07-24 16:00:20 -0700

    Completed router implementation and created edit page structure

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,9 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-/* This can check if your electron app can communicate with your backend */
-// fetch('http://localhost:3000')
-// .then(resp => resp.text())
-// .then(text => console.log(text))
-// .catch(err => {throw err})
+import './style.css';
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+import App from './components/app' ;
+
+ReactDOM.render( <App />,
+    document.getElementById( 'root' ) );
