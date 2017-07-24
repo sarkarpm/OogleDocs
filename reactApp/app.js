df29dcbbfeb9def4b49b21ea79a3f41bc7935b60
twillkens <tom.willkens@gmail.com>
2017-07-24 11:54:49 -0700

    implemented MyEditor test

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+import { MyEditor } from './MyEditor';
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
+ReactDOM.render(<MyEditor />,
    document.getElementById('root'));
