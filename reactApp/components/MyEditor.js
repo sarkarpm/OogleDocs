2a3306cb54a0018daf42f0fd92473710f7542576
Priya Sarkar <pms40@duke.edu>
2017-07-24 19:20:30 -0700

    text align

--- reactApp/components/MyEditor.js
+++ reactApp/components/MyEditor.js
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react';
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {Editor, EditorState, RichUtils, textAlignment} from 'draft-js';
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      alignment: 'left'
+    };
     this.onChange = (editorState) => this.setState({editorState});
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
   }
@@ -12,9 +15,31 @@ class MyEditor extends React.Component {
   _onBoldClick() {
     this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
   }
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  _onRightClick() {
+    this.setState({alignment: 'right'});
   }
+
+  _onLeftClick() {
+    this.setState({alignment: 'left'});
+  }
+
+  _onCenterClick() {
+    this.setState({alignment: 'center'});
+  }
+
+  myBlockStyleFn(contentBlock) {
+    if(this.state.alignment === 'left'){
+      return 'leftBlock'
+    }
+    if(this.state.alignment === 'right'){
+      return 'rightBlock'
+    }
+    if(this.state.alignment === 'center'){
+      return 'centerBlock'
+    }
+    return 'superFancyBlockquote';
+  
+}
 
   handleKeyCommand(command) {
     const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
@@ -30,8 +55,11 @@ class MyEditor extends React.Component {
       <h1>Document Title</h1>
     	<button onClick={this._onBoldClick.bind(this)}>Bold</button>
       <button onClick={this._onLeftClick.bind(this)}>Left</button>
+      <button onClick={this._onCenterClick.bind(this)}>Center</button>
+      <button onClick={this._onRightClick.bind(this)}>Right</button>
       <div className="editorBox">
       <Editor
+        blockStyleFn={this.myBlockStyleFn.bind(this)}
         editorState={this.state.editorState}
         handleKeyCommand={this.handleKeyCommand}
         onChange={this.onChange}
